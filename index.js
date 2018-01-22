fd0a392926a5c731ab72281c90a7ed46508b348d
Clément Lavigne <clement.lavigne@lu.pwc.com>
2018-01-22 10:27:53 +0100

    update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 443, () => console.log('webhook is listening'));
 
+
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
